e096249e2899d17fbddcb104c8c41f504014e245
NicoMaRojas <nicolasroro@unisabana.edu.co>
2018-04-14 16:31:45 -0500

    Envio Actividad #4

---
+++ NicolasRojas-AirCombat/Assets/Scripts/Misil1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Misil1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		this.transform.Translate(0.0f,0.0f,1.5f);
+
+	}
+
+	void OnCollisionEnter(Collision collision){
+
+		//Cuando el misil colisiona con la plataforma
+		//la elimina
+
+		Destroy (collision.collider.gameObject);
+
+		Destroy (this.gameObject);
+	}
+}
